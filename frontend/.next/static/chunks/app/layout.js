5e83e229dfa4381b293ab9698246897d40883cc5
Niko <nicolasleal73@hotmail.com>
2025-03-19 13:35:25 -0300

    still refactoring strcuture

---
+++ frontend/.next/static/chunks/app/layout.js
@@ -0,0 +1,39 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([["app/layout"],{
+
+/***/ "(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22C%3A%5C%5CUsers%5C%5CNicol%5C%5COneDrive%5C%5C%C3%81rea%20de%20Trabalho%5C%5C%E3%85%A4%5C%5Cprojetos%5C%5Cfull-stack%20projects%5C%5Ctwitter%5C%5Csrc%5C%5Capp%5C%5Cglobals.scss%22%2C%22ids%22%3A%5B%5D%7D&server=false!":
+/*!*************************************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22C%3A%5C%5CUsers%5C%5CNicol%5C%5COneDrive%5C%5C%C3%81rea%20de%20Trabalho%5C%5C%E3%85%A4%5C%5Cprojetos%5C%5Cfull-stack%20projects%5C%5Ctwitter%5C%5Csrc%5C%5Capp%5C%5Cglobals.scss%22%2C%22ids%22%3A%5B%5D%7D&server=false! ***!
+  \*************************************************************************************************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./src/app/globals.scss */ \"(app-pages-browser)/./src/app/globals.scss\"));\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL25vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvYnVpbGQvd2VicGFjay9sb2FkZXJzL25leHQtZmxpZ2h0LWNsaWVudC1lbnRyeS1sb2FkZXIuanM/bW9kdWxlcz0lN0IlMjJyZXF1ZXN0JTIyJTNBJTIyQyUzQSU1QyU1Q1VzZXJzJTVDJTVDTmljb2wlNUMlNUNPbmVEcml2ZSU1QyU1QyVDMyU4MXJlYSUyMGRlJTIwVHJhYmFsaG8lNUMlNUMlRTMlODUlQTQlNUMlNUNwcm9qZXRvcyU1QyU1Q2Z1bGwtc3RhY2slMjBwcm9qZWN0cyU1QyU1Q3R3aXR0ZXIlNUMlNUNzcmMlNUMlNUNhcHAlNUMlNUNnbG9iYWxzLnNjc3MlMjIlMkMlMjJpZHMlMjIlM0ElNUIlNUQlN0Qmc2VydmVyPWZhbHNlISIsIm1hcHBpbmdzIjoiQUFBQSxzS0FBb0oiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLz81ODY0Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCgvKiB3ZWJwYWNrTW9kZTogXCJlYWdlclwiICovIFwiQzpcXFxcVXNlcnNcXFxcTmljb2xcXFxcT25lRHJpdmVcXFxcw4FyZWEgZGUgVHJhYmFsaG9cXFxc44WkXFxcXHByb2pldG9zXFxcXGZ1bGwtc3RhY2sgcHJvamVjdHNcXFxcdHdpdHRlclxcXFxzcmNcXFxcYXBwXFxcXGdsb2JhbHMuc2Nzc1wiKTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22C%3A%5C%5CUsers%5C%5CNicol%5C%5COneDrive%5C%5C%C3%81rea%20de%20Trabalho%5C%5C%E3%85%A4%5C%5Cprojetos%5C%5Cfull-stack%20projects%5C%5Ctwitter%5C%5Csrc%5C%5Capp%5C%5Cglobals.scss%22%2C%22ids%22%3A%5B%5D%7D&server=false!\n"));
+
+/***/ }),
+
+/***/ "(app-pages-browser)/./src/app/globals.scss":
+/*!******************************!*\
+  !*** ./src/app/globals.scss ***!
+  \******************************/
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (\"c30f0f324560\");\nif (true) { module.hot.accept() }\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL3NyYy9hcHAvZ2xvYmFscy5zY3NzIiwibWFwcGluZ3MiOiI7QUFBQSwrREFBZSxjQUFjO0FBQzdCLElBQUksSUFBVSxJQUFJLGlCQUFpQiIsInNvdXJjZXMiOlsid2VicGFjazovL19OX0UvLi9zcmMvYXBwL2dsb2JhbHMuc2Nzcz83OWEwIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IFwiYzMwZjBmMzI0NTYwXCJcbmlmIChtb2R1bGUuaG90KSB7IG1vZHVsZS5ob3QuYWNjZXB0KCkgfVxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(app-pages-browser)/./src/app/globals.scss\n"));
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ __webpack_require__.O(0, ["main-app"], function() { return __webpack_exec__("(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22C%3A%5C%5CUsers%5C%5CNicol%5C%5COneDrive%5C%5C%C3%81rea%20de%20Trabalho%5C%5C%E3%85%A4%5C%5Cprojetos%5C%5Cfull-stack%20projects%5C%5Ctwitter%5C%5Csrc%5C%5Capp%5C%5Cglobals.scss%22%2C%22ids%22%3A%5B%5D%7D&server=false!"); });
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ _N_E = __webpack_exports__;
+/******/ }
+]);
---
+++ frontend/.next/static/webpack/app/(ui)/tweet/[id]/page.f30d9724a5f16c87.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/(ui)/tweet/[id]/page",{
+
+/***/ "(app-pages-browser)/./src/app/(ui)/tweet/[id]/Page.scss":
+/*!*******************************************!*\
+  !*** ./src/app/(ui)/tweet/[id]/Page.scss ***!
+  \*******************************************/
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (\"d9e3e9412c10\");\nif (true) { module.hot.accept() }\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL3NyYy9hcHAvKHVpKS90d2VldC9baWRdL1BhZ2Uuc2NzcyIsIm1hcHBpbmdzIjoiO0FBQUEsK0RBQWUsY0FBYztBQUM3QixJQUFJLElBQVUsSUFBSSxpQkFBaUIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vc3JjL2FwcC8odWkpL3R3ZWV0L1tpZF0vUGFnZS5zY3NzPzdlYzEiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgXCJkOWUzZTk0MTJjMTBcIlxuaWYgKG1vZHVsZS5ob3QpIHsgbW9kdWxlLmhvdC5hY2NlcHQoKSB9XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-pages-browser)/./src/app/(ui)/tweet/[id]/Page.scss\n"));
+
+/***/ })
+
+});
